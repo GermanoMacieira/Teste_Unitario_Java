1ed4a7c8bd993612ab5cab7f2c7f93fd5db1d31f
GermanoMacieira <germanomacieira@hotmail.com>
2023-05-31 11:18:36 -0300

    Teste com JUnit

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -28,10 +31,10 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-
 	
 	
-	public static void main(String[] args) {
+	@Test
+	public void TesteAlugarFilme() {
 		
 		//Cenário
 		LocacaoService service = new LocacaoService();
@@ -42,9 +45,29 @@ public class LocacaoService {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
 		//Verificação
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 		
 	}
+
+	
+//	Teste sem utilizar o JUnit
+//  --------------------------	
+//	public static void main(String[] args) {
+//		
+//		//Cenário
+//		LocacaoService service = new LocacaoService();
+//		Usuario usuario = new Usuario("Usuário1");
+//		Filme filme = new Filme("Filme1", 2, 5.0);
+//		
+//		//Ação
+//		Locacao locacao = service.alugarFilme(usuario, filme);
+//		
+//		//Verificação
+//		System.out.println(locacao.getValor() == 5.0);
+//		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+//		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+//		
+//	}
 }

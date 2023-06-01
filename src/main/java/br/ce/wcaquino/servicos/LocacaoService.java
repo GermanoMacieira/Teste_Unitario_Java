00a75dea230aa6e9159b947d66a9baa405f603cd
GermanoMacieira <germanomacieira@hotmail.com>
2023-06-01 12:23:33 -0300

    Comm JUnit

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -31,43 +31,48 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-	
-	
-	@Test
-	public void TesteAlugarFilme() {
-		
+
+
+// Teste SEM utilizar o JUnit
+// --------------------------
+	public static void main(String[] args) {
+
 		//Cenário
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuário1");
 		Filme filme = new Filme("Filme1", 2, 5.0);
-		
+
 		//Ação
 		Locacao locacao = service.alugarFilme(usuario, filme);
-		
+
 		//Verificação
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
+		System.out.println(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
 	}
 
-	
-//	Teste sem utilizar o JUnit
-//  --------------------------	
-//	public static void main(String[] args) {
-//		
+
+
+
+	// Teste COM utilizar o JUnit
+// --------------------------
+//	@Test
+//	public void TesteAlugarFilme() {
+//
 //		//Cenário
 //		LocacaoService service = new LocacaoService();
 //		Usuario usuario = new Usuario("Usuário1");
 //		Filme filme = new Filme("Filme1", 2, 5.0);
-//		
+//
 //		//Ação
 //		Locacao locacao = service.alugarFilme(usuario, filme);
-//		
+//
 //		//Verificação
-//		System.out.println(locacao.getValor() == 5.0);
-//		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-//		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-//		
+//		Assert.assertTrue(locacao.getValor() == 5.0);
+//		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+//		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+//
 //	}
+
 }
